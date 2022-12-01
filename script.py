5b120e10f3e7d74414d444d0ef42f94e3c68c4eb
Nicholas Lam <nicholasalexanderlam@gmail.com>
2022-12-01 15:13:34 -0500

    sent new emails

--- script.py
+++ script.py
@@ -19,9 +19,9 @@ with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
 
     subject = "Western Student Reaching Out"
 
-    df = openpyxl.load_workbook("Data.xlsx")
+    df = openpyxl.load_workbook("CAD_Data.xlsx")
     df = df.active
-    for r in range(301,400):
+    for r in range(201,300): #501 - 600
         name = df.cell(row=r, column=1).value
         if name:
             name = name.split(" ")[0].title()
